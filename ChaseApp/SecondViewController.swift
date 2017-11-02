c53c5ac312e55433541f6199c3afbd0564c0ee1c
Antonio M. Linhart <amlinhart@bv0171104.local>
2017-11-02 12:42:44 -0500

    Initial Commit

---
+++ ChaseApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ChaseApp
+//
+//  Created by Antonio M. Linhart on 11/2/17.
+//  Copyright © 2017 Antonio M. Linhart. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
