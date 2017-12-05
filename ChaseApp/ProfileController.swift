1071484633db0682f9c9ee865ef068fc7959b651
Antonio M. Linhart <amlinhart@bv0171102.EMP.BV>
2017-12-05 14:57:13 -0600

    Profile updates

--- ChaseApp/ProfileController.swift
+++ ChaseApp/ProfileController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ProfileController: UIViewController {
+class ProfileController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var profilePic: UIImageView!
     @IBOutlet weak var userField: UILabel!
@@ -22,7 +22,8 @@ class ProfileController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        profilePic.backgroundColor = UIColor.lightGray
+        
         profilePic.image = (UIImage(named:profileImage))
         profilePic.layer.cornerRadius = profilePic.frame.size.width*1/2
         profilePic.layer.borderWidth = 2.5
@@ -32,30 +33,24 @@ class ProfileController: UIViewController {
         eventLabel1.text = event1
         eventLabel2.text = event2
         
-        
-        
 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        profilePic.image = image
+        dismiss(animated: true, completion: nil)
     }
     
     
-}
-extension UIImage{
-    var circle: UIImage{
-        let square = size.width < size.height ? CGSize(width: size.width, height: size.width) : CGSize(width: size.height, height: size.height)
-        let imageView = UIImageView(frame: CGRect(origin: CGPoint(x:0, y:0), size: square))
-        imageView.contentMode = UIViewContentMode.scaleAspectFill
-        imageView.image = self
-        imageView.layer.cornerRadius = square.width/2
-        imageView.layer.masksToBounds = true
-        UIGraphicsBeginImageContext(imageView.bounds.size)
-        imageView.layer.render(in: UIGraphicsGetCurrentContext()!)
-        let result = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return result!
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = .photoLibrary
+        present(controller, animated: true, completion: nil)
+        
     }
 }
